7ee47045eadc33f15aeb813855e78bddfa7d2092
better331132 <better3311@naver.com>
2018-11-20 19:00:21 +0900

    2018-11-20 강의

--- blackjack_Class.py
+++ blackjack_Class.py
@@ -1,28 +1,43 @@
 import random
-class Card:
-    card_set = []
-    def __init__(self):
-        self.cardsort = ['Clover','Diamond','Heart','Spade']                         
-        self.cardnum = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-        for s in self.cardsort:
-            for n in self.cardnum:
-                self.card_set.append(s + n)
+def card_value(x):
+    if x == 'A':
+        return 11
+    elif x in ['0','J','Q','K']:
+        return 10
+    else :
+        return int(x)
+card_value('A')
+print(card_value('A'))
+# class Card:
+#     card_set = []
+#     def __init__(self):
+#         self.cardsort = ['Clover','Diamond','Heart','Spade']                         
+#         self.cardnum = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+#         for s in self.cardsort:
+#             for n in self.cardnum:
+#                 self.card_set.append(s + n)
+    
+# class Deck(Card):
+#     def __init__(self):
+#         random.shuffle(self.card_set)
 
-class Deck(Card):
-    def __init__(self):
-        random.shuffle(self.card_set)
+# card = Card()
+# shuffled_deck = Deck()
+# deck = shuffled_deck.card_set
+# # print(deck)
 
-card = Card()
-# print(card.card_set)
-shuffled_deck = Deck()
-deck = shuffled_deck.card_set
+# class EachDeck:
+#     poped_card = []
+#     def card_pick(self):        
+#         # print(deck)
+#         self.pick = deck.pop(0)
+#         print(self.pick)
+#         self.poped_card.append(self.pick)
+    
 
-class EachDeck:
-    poped_card = []
-    def __init__(self):
-        self.poped_card.append(deck.pop(0))
-        # self.score
+# # class DealerDeck(EachDeck):
 
-dealerdeck = EachDeck()
-dealer = dealerdeck.poped_card
-print(dealer)
+# person = EachDeck()
+# person.card_pick()
+# print(person.pick)
+# print(person.poped_card)
