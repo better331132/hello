336ef950c28b21932a34ebaf7a8946b84e0cd053
better331132 <better3311@naver.com>
2019-01-21 19:23:13 +0900

    2019-01-21

--- crawl/meltop100_image_download.py
+++ crawl/meltop100_image_download.py
@@ -10,10 +10,12 @@ heads = {
     "Referer": "https: // www.melon.com/chart/index.htm",
     "User-Agent": "Mozilla/5.0 (Macintosh Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
 }
+
 req = urllib.request.Request(url, headers = heads)
 data = urllib.request.urlopen(req).read()
 soup = BeautifulSoup(data, 'html.parser')
 images = soup.select("table img")
+
 
 for num, image in enumerate(images):
     lnk = image.get('src')
