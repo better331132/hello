28dbbc49025582a47eeb20dea8c80636cc67849f
better331132 <better3311@naver.com>
2019-02-25 14:44:58 +0900

    2019-02-25

--- bigdata/bigquery_insert.py
+++ bigdata/bigquery_insert.py
@@ -4,6 +4,7 @@ import bigquery
 import sys
 from time import time
 from google.cloud import bigquery as bq
+import json
 
 def get_conn(db):
     return pymysql.connect(
@@ -17,10 +18,18 @@ def get_conn(db):
 
 conn = get_conn('melondb')
 
+# with open('./bigquery.json','r') as key_file:
+#     json_key = json.load(key_file)
+#     pprint(json_key)
+
 client = bigquery.get_client(json_key_file='./bigquery.json', readonly=False)
-DATABASE = 'bqdb'
+print(client.project_id)
+# projectid = client.project_id
+# print(bool(client))
+# exit()
+DATABASE = 'betterbigquery'
 TABLE = 'Songs'
-print(client.check_table(DATABASE, TABLE))
+print("table exists?",client.check_table(DATABASE, TABLE))
 if not client.check_table(DATABASE, TABLE):
     client.create_table(DATABASE, TABLE,  [{"name": "songNo", 
                                             "type": "STRING"},
@@ -50,6 +59,7 @@ if not client.check_table(DATABASE, TABLE):
                                             }
                                            ]
                        )
+    
     print("Create table {0}.{1}".format(DATABASE, TABLE), file=sys.stderr)
 else:
     print("Table {} already exsits".format(TABLE))
@@ -84,41 +94,6 @@ with conn:
                           }
                 partlst.append(songdic)
             pushResult = client.push_rows(DATABASE, TABLE, partlst, insert_id_key='songNo')
-            print("Pushed Result({}) is ".format(i), pushResult) 
+            print("Pushed Result is ", pushResult) 
 end2 = time()
 print(end2-start2) # 0.87s
-# start1 = time()
-# with conn:
-#     cur = conn.cursor()
-#     for k in range(10):
-#         sql = """select s.songNo, s.songTitle,s.genre, a.* 
-#                 from Song s inner join Album a on s.albumNo = a.albumNo
-#                 limit %s offset %s""" % (str(40),str(40*k))
-#         cur.execute(sql)
-#         rows = cur.fetchall()
-#         if not rows:
-#             print("break!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-#             break
-#         else:
-#             for i, row in enumerate(rows):
-#                 # pprint(row)
-#                 # print(row['songNo'])
-#                 # print(row['albumNo'], row['albumTitle'], row['agency'], row['releaser'], row['releaseDate'], row['albumGenre'], row['rating'], row['artistNo'] )
-#                 songdic =[{ 'songNo': row['songNo'],
-#                             'songTitle': row['songTitle'],
-#                             'genre': row['genre'],
-#                             'album': {'albumNo':row['albumNo'], 
-#                                     'albumTitle':row['albumTitle'], 
-#                                     'agency':row['agency'],
-#                                     'releaser':row['releaser'],
-#                                     'releaseDate':row['releaseDate'],
-#                                     'albumGenre':row['albumGenre'],
-#                                     'rating':row['rating'],
-#                                     'artistNo': row['artistNo']}}]
-#                 pushResult = client.push_rows(DATABASE, TABLE, songdic, insert_id_key='songNo')
-#                 print("Pushed Result({}) is ".format(i), pushResult) 
-                  
-# end1 = time()
-# print(end1-start1) #41s
-
-    
